51b59edd95ac707f107472126a3b1a06740a6092
Michaël Delorme <michael.delorme@agriculture.gouv.fr>
2025-01-20 11:23:23 +0100

    feat: exercices avec version énoncées/corrigés

--- R/generer_tout.R
+++ R/generer_tout.R
@@ -2,8 +2,23 @@ library(quarto)
 
 source("R/generer_donnees.R")
 
-quarto_render("formation_quarto.qmd", output_format = "html")
 quarto_render("formation_quarto.qmd", output_format = "revealjs")
+quarto_render("formation_quarto.qmd", output_format = "html")
 quarto_render("formation_quarto.qmd", output_format = "typst")
+
+# énoncés et corrigés des exercices
+# ils sont dans un sous-répertoire : les fichiers qu'ils utilisent (dans donnees)
+# sont cependant accédés depuis la racine car on a mis execute-dir: project
+# dans _quarto_yml
+quarto_render("exercices/exercices_quarto.qmd", output_format = "html")
+quarto_render("exercices/exercices_quarto.qmd", output_format = "typst")
+
+# Les énoncés n'affichent pas le code et les solutions car on change le echo
+# avec enonces.yml
+quarto_render("exercices/exercices_quarto.qmd", 
+              metadata_file = "exercices/enonces.yml", output_format = "html")
+quarto_render("exercices/exercices_quarto.qmd", 
+              metadata_file = "exercices/enonces.yml", output_format = "typst")
+
 quarto_render("readme.qmd", output_format = "html")
 quarto_render("readme.qmd", output_format = "gfm")
