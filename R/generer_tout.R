cf1bb3903cba58ea4c63ef351b741a74e3436b1b
Michaël Delorme <michael.delorme@agriculture.gouv.fr>
2025-01-20 11:38:46 +0100

    chore

--- R/generer_tout.R
+++ R/generer_tout.R
@@ -1,24 +1,36 @@
 library(quarto)
 
+# données -----------------------------------------------------------------
+
 source("R/generer_donnees.R")
+
+
+# présentation ------------------------------------------------------------
 
 quarto_render("formation_quarto.qmd", output_format = "revealjs")
 quarto_render("formation_quarto.qmd", output_format = "html")
 quarto_render("formation_quarto.qmd", output_format = "typst")
 
-# énoncés et corrigés des exercices
+
+# énoncés et corrigés des exercices ---------------------------------------
+
 # ils sont dans un sous-répertoire : les fichiers qu'ils utilisent (dans donnees)
 # sont cependant accédés depuis la racine car on a mis execute-dir: project
 # dans _quarto_yml
+
+# corrigés
 quarto_render("exercices/exercices_quarto.qmd", output_format = "html")
 quarto_render("exercices/exercices_quarto.qmd", output_format = "typst")
 
-# Les énoncés n'affichent pas le code et les solutions car on change le echo
+# Les énoncés n'affichent pas le code et les solutions car on change le `echo`
 # avec enonces.yml
 quarto_render("exercices/exercices_quarto.qmd", 
               metadata_file = "exercices/enonces.yml", output_format = "html")
 quarto_render("exercices/exercices_quarto.qmd", 
               metadata_file = "exercices/enonces.yml", output_format = "typst")
+
+
+# lisez-moi ---------------------------------------------------------------
 
 quarto_render("readme.qmd", output_format = "html")
 quarto_render("readme.qmd", output_format = "gfm")
